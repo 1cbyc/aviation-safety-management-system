90927cd64e7180752f188e15171a30581f55b8a7
1cbyc <isaacnsisong@gmail.com>
2024-08-08 20:20:00 +0100

    refactoring asms for cli

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, render_template, request, redirect, url_for, send_from_directory
+from flask import Blueprint, render_template, request, redirect, send_from_directory, current_app
 from werkzeug.utils import secure_filename
 import pandas as pd
 from src.data_ingestion import load_data, save_data
@@ -8,12 +8,7 @@ from src.analysis import get_summary_statistics, get_safety_trends
 from src.visualization import plot_safety_trends
 from src.model import train_model
 
-# app = Flask(__name__)
 main = Blueprint('main', __name__)
-
-UPLOAD_FOLDER = 'data/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 @main.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -26,51 +21,11 @@ def upload_file():
         if file:
             filename = secure_filename(file.filename)
             # to ensure the directory exists
-            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            os.makedirs(current_app.config['UPLOAD_FOLDER'], exist_ok=True)
+            file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
             return 'File uploaded successfully'
     return render_template('upload.html')
-
-# maybe i should ditch this:
-# @main.route('/', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # to check if the file is in the request
-#         if 'file' not in request.files:
-#             return redirect(request.url)
-#
-#         file = request.files['file']
-#         if file.filename == '':
-#             return redirect(request.url)
-#
-#         if file:
-#             filename = secure_filename(file.filename)
-#             file_path = os.path.join('data/uploads', filename)
-#             file.save(file_path)
-#
-#             # to process the file
-#             df = load_data(file_path)
-#             df = clean_data(df)
-#             df = transform_data(df)
-#
-#             # to generate summary and trends
-#             summary_stats = get_summary_statistics(df)
-#             trends = get_safety_trends(df)
-#
-#             # to save the plot
-#             plot_filename = f'safety_trends_{filename.split(".")[0]}.png'
-#             plot_path = os.path.join('app/static/plots', plot_filename)
-#             plot_safety_trends(trends, save_path=plot_path)
-#
-#             return render_template('results.html', summary=summary_stats.to_html(), plot_filename=plot_filename)
-#
-#     return render_template('upload.html')
-
-
-# @main.route('/download/<filename>')
-# def download_file(filename):
-#     return send_from_directory(directory='app/static/plots', path=filename)
 
 @main.route('/download/<filename>')
 def download_file(filename):
