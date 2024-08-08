36cd4b6a54f3ccf17d5c536d7df993663f792aed
1cbyc <isaacnsisong@gmail.com>
2024-08-08 12:45:55 +0100

    asms

--- src/visualization.py
+++ src/visualization.py
@@ -20,3 +20,5 @@ def plot_safety_trends(trends):
     plt.show()
 
 # my aim here is to visualize the data to gain insights
+
+# i added the plt.show and the plt.grid and also changed location of the plt.savefig and added a plt.plot to allow it to also show popup window of the graph after running the script.
