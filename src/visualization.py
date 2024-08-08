38b413fe79ccdf96b7cde127faed82d19474d0c2
1cbyc <isaacnsisong@gmail.com>
2024-08-08 12:44:39 +0100

    asms

--- src/visualization.py
+++ src/visualization.py
@@ -3,12 +3,20 @@ import matplotlib.pyplot as plt
 def plot_safety_trends(trends):
     """plan is to plot safety trends."""
     plt.figure(figsize=(10, 6))
-    trends.plot(kind='bar')
+    plt.plot(trends.index, trends.values, marker='o')
+    # trends.plot(kind='bar')
     plt.title('Safety Trends Over Time')
     plt.xlabel('Year')
     plt.ylabel('Number of Incidents')
-    plt.savefig('plots/safety_trends.png') # to save the plotted graph to a file
-    plt.close() # to close the plot to free resources
+    # plt.savefig('plots/safety_trends.png') # to save the plotted graph to a file
+    # plt.close() # to close the plot to free resources
+    plt.grid(True)
     # plt.show()
+
+    # saving the plot
+    plt.savefig('plots/safety_trends.png') # to save the plotted graph to a file
+
+    # show the plot
+    plt.show()
 
 # my aim here is to visualize the data to gain insights
