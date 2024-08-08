e7087ae78130e8d64e369bc16562dc0e514ecda7
1cbyc <isaacnsisong@gmail.com>
2024-08-08 06:36:56 +0100

    asms

--- src/analysis.py
+++ src/analysis.py
@@ -6,6 +6,8 @@ def get_summary_statistics(df):
 
 def get_safety_trends(df):
     """to identify safety trends over time"""
+    # now to ensure the incident_data column is in datetime format
+    df['incident_date'] = pd.to_datetime(df['incident_date'])
     trends = df.groupby(df['incident_date'].dt.year).size()
     return trends
 
