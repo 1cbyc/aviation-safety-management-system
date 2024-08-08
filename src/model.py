e85c3fcb569659ec39028f2067c48e5f348ba3fd
1cbyc <isaacnsisong@gmail.com>
2024-08-08 06:26:14 +0100

    asms

--- src/model.py
+++ src/model.py
@@ -1,16 +1,33 @@
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.metrics import classification_report
+#
+# def train_model(X, y):
+#     """to train the ML model"""
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#     model = LogisticRegression()
+#     model.fit(X_train, y_train)
+#     y_pred = model.predict(X_test)
+#     print(classification_report(y_test, y_pred))
+#     return model
+#
+# # my aim is to implement machine learning models to predict risk factors.
+
+# let me update totally:
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
+
 def train_model(X, y):
-    """to train the ML model"""
+    if y.dtype == 'object':
+        le = LabelEncoder()
+        y = le.fit_transform(y)
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = LogisticRegression()
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    print(classification_report(y_test, y_pred))
-    return model
 
-# my aim is to implement machine learning models to predict risk factors.
+    joblib.dump(model, 'models/logistic_regression_model.pkl')
+    return model
