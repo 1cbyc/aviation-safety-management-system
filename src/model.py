7254cca2ae7060b65e43eae8b0939ae94535a50d
1cbyc <isaacnsisong@gmail.com>
2024-08-08 05:55:37 +0100

    asms

---
+++ src/model.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+def train_model(X, y):
+    """to train the ML model"""
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    return model
+
+# my aim is to implement machine learning models to predict risk factors.
