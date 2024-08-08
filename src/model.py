c9e60161af19bba2f30b97e1a8dfa3c23b471fb3
1cbyc <isaacnsisong@gmail.com>
2024-08-08 12:39:23 +0100

    asms

--- src/model.py
+++ src/model.py
@@ -14,11 +14,11 @@
 # # my aim is to implement machine learning models to predict risk factors.
 
 # let me update totally:
+import os
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import joblib
-
 
 def train_model(X, y):
     """to train the ml model now"""
@@ -36,6 +36,13 @@ def train_model(X, y):
     model = LogisticRegression()
     model.fit(X_train, y_train)
 
+    # help me create the 'models' directory if it does not exist
+    model_dir = 'models'
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
     # to save the trained model
-    joblib.dump(model, 'models/logistic_regression_model.pkl')
+    model_path = os.path.join(model_dir, 'logistic_regression_model.pkl')
+    joblib.dump(model, model_path)
+    # joblib.dump(model, 'models/logistic_regression_model.pkl')
     return model
