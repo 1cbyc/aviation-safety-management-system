366947779ebc37625cf951904bae7aef1fcf24aa
1cbyc <isaacnsisong@gmail.com>
2024-08-08 20:50:15 +0100

    refactoring asms for cli

---
+++ web/src/visualization.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def plot_safety_trends(trends, save_path=None):
+    plt.figure(figsize=(10, 6))
+    plt.plot(trends['date'], trends['incident_count'], marker='o', linestyle='-')
+    plt.title('Safety Trends Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Number of Incidents')
+    plt.grid(True)
+
+    if save_path:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+    plt.close()
