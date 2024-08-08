07a8ab32ced07df4d339c41d439ab37bfb985d76
1cbyc <isaacnsisong@gmail.com>
2024-08-08 20:01:18 +0100

    refactoring asms for cli

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -2,11 +2,11 @@ import os
 from flask import Blueprint, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import pandas as pd
-from ...src.data_ingestion import load_data, save_data
-from ...src.data_preprocessing import clean_data, transform_data
-from ...src.analysis import get_summary_statistics, get_safety_trends
-from ...src.visualization import plot_safety_trends
-from ...src.model import train_model
+from src.data_ingestion import load_data, save_data
+from src.data_preprocessing import clean_data, transform_data
+from src.analysis import get_summary_statistics, get_safety_trends
+from src.visualization import plot_safety_trends
+from src.model import train_model
 
 main = Blueprint('main', __name__)
 
