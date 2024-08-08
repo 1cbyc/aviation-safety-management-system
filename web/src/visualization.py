3c069ed5da29392903bc9ff7517739059f63ae50
1cbyc <isaacnsisong@gmail.com>
2024-08-08 20:38:04 +0100

    refactoring asms for cli

--- web/src/visualization.py
+++ web/src/visualization.py
@@ -3,15 +3,22 @@ import matplotlib.pyplot as plt
 def plot_safety_trends(trends, save_path):
     """to plot the safety trends over time and save"""
     plt.figure(figsize=(10, 6))
-    plt.plot(trends.index, trends.values, marker='o')
-    plt.plot(trends['date'], trends['incident_count'], marker='o', )
+    # plt.plot(trends.index, trends.values, marker='o')
+    plt.plot(trends['date'], trends['incident_count'], marker='o', linestyle='-')
     plt.title('Safety Trends Over Time')
-    plt.xlabel('Year')
+    # plt.xlabel('Year')
+    plt.xlabel('Date')
     plt.ylabel('Number of Incidents')
     plt.grid(True)
 
-    # to save the plot
-    plt.savefig(save_path)
+    if save_path:
+        plt.savefig(save_path)
+    else:
+        plt.show()
 
-    # not needed but can be used to show the plot if running locally
-    plt.show()
+    plt.close()
+    # # to save the plot
+    # plt.savefig(save_path)
+    #
+    # # not needed but can be used to show the plot if running locally
+    # plt.show()
