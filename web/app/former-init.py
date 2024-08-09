4d55355a858d813ac9d59426b30a92e68036d7c2
1cbyc <isaacnsisong@gmail.com>
2024-08-09 09:21:46 +0100

    building the asms for web

---
+++ web/app/former-init.py
@@ -0,0 +1,11 @@
+# from flask import Flask
+#
+# def create_app():
+#     app = Flask(__name__)
+#     app.config['UPLOAD_FOLDER'] = 'data/uploads'
+#     app.config['PLOTS_FOLDER'] = 'app/static/plots'
+#
+#     from .routes import main
+#     app.register_blueprint(main)
+#
+#     return app
