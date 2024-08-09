d4d31909c7bca1a7168a674488a86eced60f29dd
1cbyc <isaacnsisong@gmail.com>
2024-08-09 09:15:25 +0100

    building the asms for web

--- web/run.py
+++ web/run.py
@@ -1,6 +1,17 @@
 # import sys
 # import os
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# from app import create_app
+#
+# app = create_app()
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+#
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
+
 from app import create_app
 
 app = create_app()
