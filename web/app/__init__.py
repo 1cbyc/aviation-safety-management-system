9b4e141bc447c475112eeccf9e571772eb23602b
1cbyc <isaacnsisong@gmail.com>
2024-08-09 09:14:25 +0100

    building the asms for web

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -13,14 +13,19 @@
 import os
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# to set configuration paths
-app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
-app.config['PLOTS_FOLDER'] = os.path.join(app.root_path, 'static', 'plots')
+    # to set configuration paths
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
+    app.config['PLOTS_FOLDER'] = os.path.join(app.root_path, 'static', 'plots')
 
-# to ensure the directories exist
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-os.makedirs(app.config['PLOTS_FOLDER'], exist_ok=True)
+    # to ensure the directories exist
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    os.makedirs(app.config['PLOTS_FOLDER'], exist_ok=True)
 
-#
+    # to register blueprints
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
