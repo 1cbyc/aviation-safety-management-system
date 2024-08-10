6c4a9ce9cd511ddbce35ad99f35c8956eeb357f5
1cbyc <isaacnsisong@gmail.com>
2024-08-10 23:22:17 +0100

    building the asms for web

--- web/run.py
+++ web/run.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 # Configure the paths for file uploads and plot storage
 app.config['UPLOAD_FOLDER'] = 'app/uploads'
 app.config['PLOTS_FOLDER'] = 'app/static/plots'
-# app.config['UPLOAD_FOLDER'] = 'web/app/uploads'
-# app.config['PLOTS_FOLDER'] = 'web/app/static/plots'
 
 # Register the Blueprint
 app.register_blueprint(main)
