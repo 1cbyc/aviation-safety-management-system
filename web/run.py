ec0ad9f5a127f16864e9f49709f088c0da56e6b1
1cbyc <isaacnsisong@gmail.com>
2024-08-10 23:53:55 +0100

    building the asms for web

--- web/run.py
+++ web/run.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from web.app.routes import main
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__, template_folder='app/templates')  # Ensure this path is correct
 # Configure the paths for file uploads and plot storage
 app.config['UPLOAD_FOLDER'] = 'app/uploads'
